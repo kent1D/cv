502608c51709d96c7f88ac0e2e00c2a3314720e4
kent1 <kent1@arscenic.info>
2013-10-14 13:22:22 +0200

    Et si on créait 4 rubriques dès le départ :
    - Expérience
    - Formation
    - Projets
    - Portfolio
    
    On ajoute un nécessite sur compositions qui permettra de gérer
    correctement ces 4 cas

--- cv_administrations.php
+++ cv_administrations.php
@@ -14,12 +14,55 @@ function cv_upgrade($nom_meta_base_version, $version_cible){
 	
 	// Première installation
 	$maj['create'] = array(
+		array('cv_configuration_base',true),
+		array('cv_creer_rubriques',true)
 	);
 	
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
+function cv_configuration_base(){
+	/**
+	 * Désactivation de fontionnalités du privé:
+	 * -* la messagerie
+	 * -* les forums
+	 */
+	ecrire_meta("messagerie_agenda", "non");
+	ecrire_meta("forum_prive_admin","non");
+	ecrire_meta("forum_prive_objets","non");
+	ecrire_meta("forum_prive","non");
+	
+	/**
+	 * Ne pas activer les inscriptions de rédacteurs
+	 */
+	ecrire_meta("accepter_inscriptions","non");
+	
+	/**
+	 * Activation des documents sur les articles
+	 */
+	ecrire_meta("documents_objets", implode(',',array('spip_articles')));
+}
+
+function cv_creer_rubriques(){
+	include_spip('action/editer_rubrique');
+	if(!sql_getfetsel('id_rubrique','spip_rubriques','composition="experience"')){
+		$rub_experience = insert_rubrique(0);
+		revisions_rubriques($rub_experience, array('titre' =>'1. '._T('cv:titre_rubrique_experience'),'composition'=>'experience','agenda'=>1));
+	}
+	if(!sql_getfetsel('id_rubrique','spip_rubriques','composition="formation"')){
+		$rub_formation = insert_rubrique(0);
+		revisions_rubriques($rub_formation, array('titre' =>'2. '._T('cv:titre_rubrique_formation'),'composition'=>'formation','agenda'=>1));
+	}
+	if(!sql_getfetsel('id_rubrique','spip_rubriques','composition="projets"')){
+		$rub_projets = insert_rubrique(0);
+		revisions_rubriques($rub_projets, array('titre' =>'3. '._T('cv:titre_rubrique_projets'),'composition'=>'projets','agenda'=>1));
+	}
+	if(!sql_getfetsel('id_rubrique','spip_rubriques','composition="portfolio"')){
+		$rub_portfolio = insert_rubrique(0);
+		revisions_rubriques($rub_portfolio, array('titre' =>'4. '._T('cv:titre_rubrique_portfolio'),'composition'=>'portfolio','agenda'=>1));
+	}
+}
 /**
  * Desinstallation/suppression des tables cv
  *
@@ -27,6 +70,7 @@ function cv_upgrade($nom_meta_base_version, $version_cible){
  */
 function cv_vider_tables($nom_meta_base_version) {
 	effacer_meta($nom_meta_base_version);
+
 	// Effacer la config
 	effacer_meta('cv');
 }
